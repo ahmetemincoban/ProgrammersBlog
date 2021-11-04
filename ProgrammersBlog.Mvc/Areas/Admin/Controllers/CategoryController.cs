1b94ddb78aff0e8794058bd0ddeb8e644dd977c5
ahmetemincoban <ezcrashae@gmail.com>
2021-11-05 01:15:29 +0300

    Preparing for AJAX Post

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Mvc.Areas.Admin.Models;
 using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.Complex_Types;
 
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
@@ -23,10 +26,18 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
             var result = await _categoryService.GetAll();
             return View(result.Data);
         }
-
+        [HttpGet]
         public IActionResult Add()
         {
             return PartialView("_CategoryAddPartial");
+        }
+        [HttpPost]
+        public async IActionResult Add(CategoryAddDto categoryAddDto)
+        {
+            var categoryAjaxModel = new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto),
+            };
         }
     }
 }
